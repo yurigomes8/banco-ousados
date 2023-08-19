e28c054de56cf37f9b7c32f288b560544319e65d
yuri.gomes <yuri.gomes@compasso.com.br>
2023-08-19 00:38:55 -0300

    First commit skeleton

---
+++ src/main/java/br/com/ousadoscorporation/bancoousados/BancoOusadosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ousadoscorporation.bancoousados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoOusadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoOusadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ousadoscorporation/bancoousados/BancoOusadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ousadoscorporation.bancoousados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoOusadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
